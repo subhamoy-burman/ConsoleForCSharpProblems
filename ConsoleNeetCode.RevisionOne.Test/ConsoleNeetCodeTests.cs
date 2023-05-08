a42cb67fb9aa008c48bf074b6a0404e1c1f41a2b
Burman, Subhamoy <subhamoy.burman@medtronic.com>
2023-05-08 12:39:38 +0530

    DP Changes

--- ConsoleNeetCode.RevisionOne.Test/ConsoleNeetCodeTests.cs
+++ ConsoleNeetCode.RevisionOne.Test/ConsoleNeetCodeTests.cs
@@ -18,5 +18,32 @@ namespace ConsoleNeetCode.RevisionOne.Test
             var stairs = DynamicProgramming.DynamicProgramming.ClimbingStairsRecursionCount(input);
             Assert.AreEqual(stairs,expectedResult);
         }
+
+        [Test]
+        [TestCase(new int[] {1,2,3,1},4)]
+        [TestCase(new int[] {2,7,9,3,1},12)]
+        public void TestHouseRobber(int[] arr, int expectedResult)
+        {
+            var maxRobbed = DynamicProgramming.DynamicProgramming.HouseRobber(arr);
+            Assert.AreEqual(maxRobbed, expectedResult);
+        }
+        
+        [Test]
+        [TestCase(new int[] {1,2,3,1},4)]
+        [TestCase(new int[] {2,7,9,3,1},12)]
+        public void TestHouseRobberDp(int[] arr, int expectedResult)
+        {
+            var maxRobbed = DynamicProgramming.DynamicProgramming.HouseRobberDP(arr);
+            Assert.AreEqual(maxRobbed, expectedResult);
+        }
+        
+        [Test]
+        [TestCase(new int[] {1,2,3,1},4)]
+        [TestCase(new int[] {2,7,9,3,1},12)]
+        public void TestHouseRobberMemo(int[] arr, int expectedResult)
+        {
+            var maxRobbed = DynamicProgramming.DynamicProgramming.HouseRobberMemo(arr);
+            Assert.AreEqual(maxRobbed, expectedResult);
+        }
     }
 }
--- ConsoleNeetCode.RevisionOne/DynamicProgramming/DynamicProgramming.cs
+++ ConsoleNeetCode.RevisionOne/DynamicProgramming/DynamicProgramming.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 
 namespace ConsoleNeetCode.RevisionOne.DynamicProgramming
@@ -36,6 +37,74 @@ namespace ConsoleNeetCode.RevisionOne.DynamicProgramming
 
         public static int MinCostInClimbingNoOfStairs(int[] costOfClimbing)
         {
+            return 1;
+        }
+
+        public static int HouseRobber(int[] houseList)
+        {
+            var maxRobbed = HouseRobberSolve(houseList, houseList.Length-1);
+            return maxRobbed;
+        }
+        
+        public static int HouseRobberDP(int[] houseList)
+        {
+            int[] dpArray = new int[houseList.Length];
+            var maxRobbed = HouseRobberSolveDP(houseList, dpArray);
+            return maxRobbed;
+        }
+        
+        public static int HouseRobberMemo(int[] houseList)
+        {
+            int[] dpArray = new int[houseList.Length];
+            Array.Fill(dpArray,-1);
+            var maxRobbed = HouseRobberSolveMemo(houseList, houseList.Length - 1, dpArray);
+            return maxRobbed;
+        }
+
+        private static int HouseRobberSolve(int[] houseList, int houseIndex)
+        {
+            if (houseIndex < 0)
+            {
+                return 0;
+            }
+
+            if (houseIndex == 0 || houseIndex == 1)
+            {
+                return houseList[houseIndex];
+            }
+
+            return Math.Max(HouseRobberSolve(houseList, houseIndex-1), HouseRobberSolve(houseList, houseIndex-2) + houseList[houseIndex]);
+        }
+
+        private static int HouseRobberSolveDP(int[] houseList, int[] dpArray)
+        {
+            dpArray[0] = houseList[0];
+            dpArray[1] = houseList[1];
+            for (int i = 2; i < dpArray.Length; i++)
+            {
+                dpArray[i] = Math.Max(dpArray[i - 1], dpArray[i - 2] + houseList[i]);
+            }
+
+            return dpArray[houseList.Length-1];
+        }
+
+        private static int HouseRobberSolveMemo(int[] houseList, int index, int[] dpArray)
+        {
+            if (index == 0 || index == 1)
+            {
+                return dpArray[index] = houseList[index];
+            }
+            
+            if (dpArray[index] != -1)
+            {
+                return dpArray[index];
+            }
+
+            int result = Math.Max(HouseRobberSolveMemo(houseList, index-1, dpArray),
+                HouseRobberSolveMemo(houseList, index - 2 , dpArray)+ houseList[index]);
+
+            dpArray[index] = result;
+            return result;
         }
     }
     
