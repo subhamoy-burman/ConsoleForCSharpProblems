1236f4b98d8ef24ddd00fcf43c1fa9d9a1349398
ENT\burmas3 <subhamoy.burman@medtronic.com>
2023-06-30 17:27:45 +0530

    Tree

---
+++ ConsoleNeetCode.RevisionOne/Graphs/GraphRevision.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleNeetCode.RevisionOne.Graphs;
+
+public static class GraphRevision
+{
+    public class WordLevel
+    {
+        public int Level { get; set; }
+        public string Word { get; set; }
+
+        public WordLevel(int level, string word)
+        {
+            Level = level;
+            Word = word;
+        }
+    }
+    public static int WordLadderLength(string startWord, string endWord, string[] wordList)
+    {
+        Queue<WordLevel> wordLevels = new Queue<WordLevel>();
+        HashSet<string> uniqueStrings = new HashSet<string>();
+
+        foreach (var item in wordList)
+        {
+            uniqueStrings.Add(item);
+        }
+        
+        wordLevels.Enqueue(new WordLevel(1, startWord));
+
+        while (wordLevels.Count>0)
+        {
+            var current = wordLevels.Dequeue();
+            var currentLevel = current.Level;
+            var currentWord = current.Word;
+
+            if (currentWord == endWord) return currentLevel;
+
+            for (int i = 0; i < currentWord.Length; i++)
+            {
+                for (char ch = 'a'; ch <= 'z'; ch++)
+                {
+                    var charArray = currentWord.ToCharArray();
+                    charArray[i] = ch;
+                    var newWord = new string(charArray);
+                    
+                    if (uniqueStrings.Contains(newWord))
+                    {
+                        uniqueStrings.Remove(newWord);
+                        wordLevels.Enqueue(new WordLevel(currentLevel+1, newWord));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ ConsoleNeetCode.RevisionOne/Trees/TreeRevision.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleNeetCode.RevisionOne.Trees;
+
+public static class TreeRevision
+{
+    public class NodeVLevel
+    {
+        public int VerticalLevel { get; set; }
+        public TreeNodeRev Node { get; set; }
+    }
+
+    public class TreeNodeRev
+    {
+        public int Val;
+        public TreeNodeRev Left;
+        public TreeNodeRev Right;
+
+        public TreeNodeRev(int val = 0, TreeNodeRev left = null, TreeNodeRev right = null)
+        {
+            this.Val = val;
+            this.Left = left;
+            this.Right = right;
+        }
+    }
+
+    public static List<int> TopViewOnBinaryTree(TreeNodeRev treeNode)
+    {
+        Queue<NodeVLevel> nodeVLevels = new Queue<NodeVLevel>();
+        nodeVLevels.Enqueue(new NodeVLevel()
+        {
+            Node = treeNode,
+            VerticalLevel = 0
+        });
+
+        Dictionary<int, TreeNodeRev> levelDictionary = new Dictionary<int, TreeNodeRev>();
+
+        while (nodeVLevels.Count != 0)
+        {
+            var currentNode = nodeVLevels.Dequeue();
+
+            if (!levelDictionary.ContainsKey(currentNode.VerticalLevel))
+            {
+                levelDictionary.Add(currentNode.VerticalLevel, currentNode.Node);
+            }
+
+            if (currentNode.Node.Left != null)
+            {
+                nodeVLevels.Enqueue(new NodeVLevel()
+                {
+                    Node = currentNode.Node.Left,
+                    VerticalLevel = currentNode.VerticalLevel - 1
+                });
+            }
+
+            if (currentNode.Node.Right != null)
+            {
+                nodeVLevels.Enqueue(new NodeVLevel()
+                {
+                    Node = currentNode.Node.Right,
+                    VerticalLevel = currentNode.VerticalLevel + 1
+                });
+            }
+        }
+
+        List<int> sortedKeys = levelDictionary.Keys.ToList();
+        sortedKeys.Sort();
+        List<int> verticalLevel = new List<int>();
+
+        foreach (var key in sortedKeys)
+        {
+            verticalLevel.Add(levelDictionary[key].Val);
+        }
+
+        return verticalLevel;
+    }
+    
+    public static List<int> BottomViewOfBinaryTree(TreeNodeRev treeNode)
+    {
+        Queue<NodeVLevel> nodeVLevels = new Queue<NodeVLevel>();
+        nodeVLevels.Enqueue(new NodeVLevel()
+        {
+            Node = treeNode,
+            VerticalLevel = 0
+        });
+
+        Dictionary<int, TreeNodeRev> levelDictionary = new Dictionary<int, TreeNodeRev>();
+
+        while (nodeVLevels.Count != 0)
+        {
+            var currentNode = nodeVLevels.Dequeue();
+
+            if (!levelDictionary.ContainsKey(currentNode.VerticalLevel))
+            {
+                levelDictionary.Add(currentNode.VerticalLevel, currentNode.Node);
+            }
+            else
+            {
+                levelDictionary[currentNode.VerticalLevel] = currentNode.Node;
+            }
+
+            if (currentNode.Node.Left != null)
+            {
+                nodeVLevels.Enqueue(new NodeVLevel()
+                {
+                    Node = currentNode.Node.Left,
+                    VerticalLevel = currentNode.VerticalLevel - 1
+                });
+            }
+
+            if (currentNode.Node.Right != null)
+            {
+                nodeVLevels.Enqueue(new NodeVLevel()
+                {
+                    Node = currentNode.Node.Right,
+                    VerticalLevel = currentNode.VerticalLevel + 1
+                });
+            }
+        }
+
+        List<int> sortedKeys = levelDictionary.Keys.ToList();
+        sortedKeys.Sort();
+        List<int> verticalLevel = new List<int>();
+
+        foreach (var key in sortedKeys)
+        {
+            verticalLevel.Add(levelDictionary[key].Val);
+        }
+
+        return verticalLevel;
+    }
+}
